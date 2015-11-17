18149d8b8e87ef4d57e59e20f1393f0c6eef9870
nickzuber <nickzoober@gmail.com>
2015-11-17 11:51:38 -0500

    Dynamic web page rendering
    Load and render web pages through the use of history API and AJAX to
    provide a seamless and smooth transition. Jaz takes predetermined
    contents of web pages and dynamically renders them onto the current
    page, while asynchronously utilizing history API to navigate the user to
    that web page, ultimately providing a smooth page transition.

---
+++ scripts/http/simple_server.py
@@ -0,0 +1,21 @@
+# simple_server.py
+# ================
+# runs a simple server for tests and builds
+
+import SimpleHTTPServer
+import SocketServer
+import BaseHTTPServer
+import os
+
+HOST = "localhost"
+
+PORT = 8080
+
+HANDLER = SimpleHTTPServer.SimpleHTTPRequestHandler
+
+os.chdir("C:\~projects\jaz")
+
+httpd = SocketServer.TCPServer((HOST, PORT), HANDLER)
+
+print ("Serving at: http://%s:%s" % (HOST, PORT))
+httpd.serve_forever()
