62f32db90a0415c1e767b48bd60ec7c249870b43
nickzuber <nickzoober@gmail.com>
2015-11-17 23:04:44 -0500

    minor bug fixes

--- test/scripts/jaz.js
+++ test/scripts/jaz.js
@@ -2,6 +2,30 @@
  *  Jaz | main entry point
  */
 
-class jaz {
-	
+const Scope = function(){
+  
+}
+
+const Transition = function(){
+  
+}
+
+
+
+/**
+ * Constuct base object with respective private data
+ * @attribute scope which links are to be affected by Jaz
+ * @attribute transition callback function that executes while new page is rendering
+ */
+const Jaz = function(){
+  this.scope = new Scope();
+  this.transition = new Transition();
+};
+
+Jaz.prototype.remoteBlockRouting = function(){
+  if(typeof this.scope == 'object' && this.scope){
+    console.log('invoked');
+  }else{
+    throw new Error("Error: undefined or invalid scope.");
+  }
 }
