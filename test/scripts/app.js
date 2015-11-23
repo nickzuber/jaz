898ad3bea758809985313dfdf8e1d42d965df6e0
nickzuber <nickzoober@gmail.com>
2015-11-23 15:52:50 -0500

    added a few testing details

--- test/scripts/app.js
+++ test/scripts/app.js
@@ -8,12 +8,14 @@ require(['Jaz'], function(Jaz){
       var waiting = document.createElement('div');
       waiting.className = 'waiting';
       document.body.appendChild(waiting);
+      document.querySelector('#nav-header').innerHTML = '<img src="img/loading.gif" style="opacity:.25;" height="35px" />';
     }
   }
 
   function loadedFunction(){
     if(document.querySelector('.waiting')){
       document.querySelector('.waiting').remove();
+      document.querySelector('#nav-header').innerHTML = 'Home';
     }
   }
 
